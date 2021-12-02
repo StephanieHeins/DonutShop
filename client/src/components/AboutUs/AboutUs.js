029ae7f399a30317463f0f54be8db7ef7dada58d
Steph Heins <stephanie_heins@yahoo.com>
2021-12-01 21:58:45 -0800

    Added components/AboutUs

---
+++ client/src/components/AboutUs/AboutUs.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const AboutUs = () => {
+    return (
+        
+    <div className="container">
+        <div className="columns">
+            <div className="column is-four-fifths is-offset-1 mb-4">
+            <div className="has-text-centered">
+            ~ Here at Homemade Donuts, we take pride in only using ingredients that you can recognize. 
+            Our dough is made with a mix of flour, egg yolks, vanilla, shortening, baking powder, salt, 
+            sugar, water, and yeast.  Our frosting and glazes are made with powdered sugar and organic 
+            extracts.
+            All of our donuts are made fresh daily by our staff. Please keep checking back here or on 
+            our socials as we are constantly adding new flavors as well as gluten free, vegan and dairy-free 
+            options. Enjoy! ~
+            </div>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default AboutUs;
